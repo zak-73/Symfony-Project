67a2b9e070c31af78f6c9494f9d891abad0d5760
Zakaria Charmig <zakaria.charmig@gaea21.org>
2021-10-15 00:10:52 +0200

    test_symfony

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,12 @@
-
 /*
  * Welcome to your app's main JavaScript file!
  *
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-
+import React from 'react';
+//import npm install react react-dom from 'react-dom';
+//import { BrowserRouter as Router } from 'react-router-dom';
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
 
